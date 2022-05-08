eab53ec06e316e625254b7876898378f144c07ab
Taras Bondar <strelezzzz@gmail.com>
2022-05-08 17:44:21 +0300

    7.2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryImg = createImgMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
+
+function createImgMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                        <a class="gallery__link" href="${original}">
+                            <img
+                                class="gallery__image"
+                                src="${preview}"
+                                data-source="${original}"
+                                alt="${description}"
+                            />
+                        </a>
+                    </div>
+                    `;
+    })
+    .join("");
+}
