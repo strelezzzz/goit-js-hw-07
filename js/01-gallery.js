48e990c78975819057d3d4145e855a45fa3dd53e
Taras Bondar <strelezzzz@gmail.com>
2022-05-08 17:38:37 +0300

    Update 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 // 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 
-const galleryContainer = document.querySelector('.gallery')
+const galleryContainer = document.querySelector(".gallery");
 const galleryImg = createImgMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', galleryImg);
+galleryContainer.insertAdjacentHTML("beforeend", galleryImg);
 
 function createImgMarkup(images) {
-    return images.map(({ preview, original, description }) => {
-            return `<div class="gallery__item">
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
                         <a class="gallery__link" href="${original}">
                             <img
                                 class="gallery__image"
@@ -19,28 +20,35 @@ function createImgMarkup(images) {
                         </a>
                     </div>
                     `;
-        })
-        .join('');      
+    })
+    .join("");
 }
 // 2.Реалізація делегування на div.gallery і отримання url великого зображення.
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+window.addEventListener("keydown", onEscapePress);
+
+let modalWindow;
 
 function onGalleryContainerClick(e) {
-    e.preventDefault();
-    if (!e.target.classList.contains('gallery__image')) {
-        return;
-    }
-    const imageFocusSource = e.target.dataset.source;
-    // 4.Відкриття модального вікна по кліку на елементі галереї.
-    const modalWindow = basicLightbox.create(`<img width='1280' heigth = '800' 
-    src ="${imageFocusSource}" >
-    `)
+  e.preventDefault();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const imageFocusSource = e.target.dataset.source;
+  // 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
+  // Використовуй CDN сервіс jsdelivr і додай у проект посилання
+  // на мініфіковані(.min) файли бібліотеки.
 
-    modalWindow.show();
+  // 4.Відкриття модального вікна по кліку на елементі галереї.
+  modalWindow =
+    basicLightbox.create(`<img src ="${imageFocusSource}" width='1280' heigth = '800' alt = "This is image :)" >
+    `);
+  modalWindow.show();
 }
-// 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox.
-// Використовуй CDN сервіс jsdelivr і додай у проект посилання
-// на мініфіковані(.min) файли бібліотеки.
 
-
-
+function onEscapePress(event) {
+  if (event.code === "Escape") {
+    window.removeEventListener;
+    modalWindow.close();
+  }
+}
